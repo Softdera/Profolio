614123c619969ca951f300fac4890453b54b6e60
Softdera <126988761+Softdera@users.noreply.github.com>
2024-09-15 12:33:09 +0100

    Update About.vue

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
       <div class="about"> 
         <h2>About Me</h2>
       <p>I am a graduate of the University of Nigeria, Nsukka with a keen interest in frontend development and enjoy bringing designs to life using modern web technologies and Artificial Intelligence.</p>
-        <p> I develop both code and no code systems </p>
+        <p> I develop both code and no/low code systems </p>
   </div> 
     </section>
   </template>
