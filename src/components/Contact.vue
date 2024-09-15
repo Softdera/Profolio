a66fd507002eaad2b452a40ab11fd9357bdee025
Softdera <126988761+Softdera@users.noreply.github.com>
2024-09-15 12:47:58 +0100

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,7 +2,7 @@
     <section>
       <h2>Contact Me</h2>
       <label>Email:</label>  <a href="mailto:chiekwenachidera@gmail.com"> chiekwenachidera@gmail.com</a> 
-       <p> <label>Mobile:</label> 08138134923 </p> 
+       <p> <label>Mobile:</label> <a href="tel:+2348138134923"> 08138134923 </a> </p> 
      
     </section>
   </template>
